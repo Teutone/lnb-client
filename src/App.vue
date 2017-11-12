6572c4853270f8d88d35b3ba9a40cb27f8ece02d
Benjamin Bädorf <hello@benjaminbaedorf.com>
2017-11-12 02:23:37 +0100

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<script>
+import LnbHeader from './components/LnbHeader';
+
+export default {
+  name: 'app',
+  components: {
+    LnbHeader,
+  },
+
+  created() {
+    document.title = 'latenight.blue';
+  },
+};
+</script>
+
+<style src="./assets/scss/global.scss" lang="scss"></style>
+<style src="./assets/latenight.blue/scss/index.scss" lang="scss"></style>
+
+<template>
+  <main id="main">
+    <lnb-header></lnb-header>
+    <lnb-song></lnb-song>
+  </main>
+</template>
